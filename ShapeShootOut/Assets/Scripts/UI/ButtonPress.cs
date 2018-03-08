82582c473981eb256ff7c2668f9deb603a6798c5
vinnchenzo94 <vao1amt@bolton.ac.uk>
2018-03-08 18:42:02 +0000

    UI & Post Proc

---
+++ ShapeShootOut/Assets/Scripts/UI/ButtonPress.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonPress : MonoBehaviour {
+
+    public string scene_to_load;
+
+    public void Scene_Changer (string _scene_name) {
+        SceneManager.LoadScene(_scene_name);
+    }
+}
